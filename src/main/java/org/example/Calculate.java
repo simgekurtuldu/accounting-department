f548f26fa250bda29a3b57ad0ec55cb7d2338c8b
VFTR\amdocsskurtuldu <simge.kurtuldu@vodafone.com>
2023-12-29 07:31:12 +0300

    add Calculate class

---
+++ src/main/java/org/example/Calculate.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calculate {
+    boolean bonus;
+    int monthlyBonus;
+
+    public void calculateSalary(String employeeName, int numberOfDaysWorked, int dailyWage){
+        if(numberOfDaysWorked > 25){
+            bonus = true;
+            calculateBonus(numberOfDaysWorked);
+            System.out.println("Sevgili " + employeeName + " bu ay " + monthlyBonus + " TL prim kazandınız. Tebrikler!");
+        }
+        System.out.println(employeeName + " çalışanının aylık maaşı: " + dailyWage*numberOfDaysWorked);
+        if(bonus){
+            System.out.println(employeeName + " çalışanının prim ile birlikte aylık toplam maaşı: " + ((dailyWage*numberOfDaysWorked) + monthlyBonus));
+            bonus = false;
+        }
+    }
+    public int calculateBonus( int numberOfDaysWorked){
+        return monthlyBonus = (numberOfDaysWorked - 25) * 1000;
+    }
+}
